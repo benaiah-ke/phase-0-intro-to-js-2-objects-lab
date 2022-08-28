8393ed98906c058a7fa4c469867960786cf2781b
benaiah-ke <benaiahpraise@gmail.com>
2022-08-28 12:39:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+let employee = {name:'Praise', streetAddress:'Entomoto Drive'};
+
+function updateEmployeeWithKeyAndValue (employee,key,value) {
+    let employee1 = {...employee};
+    employee1[key] = value;
+
+    return employee1;
+};
+// console.log (updateEmployeeWithKeyAndValue(employee,'height',11));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    employee[key] = value;
+    return employee;
+};
+
+// console.log (destructivelyUpdateEmployeeWithKeyAndValue(employee,'height',11));
+
+function deleteFromEmployeeByKey(employee,key) {
+    let employee2 = {...employee};
+    delete employee2[key];
+    
+    return employee2;
+};
+
+// console.log (deleteFromEmployeeByKey(employee,'name'));
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
